c9cc58be1ce0f8d00b2cbfa2b87ad974eeffd106
hxck <embercodex@proton.me>
2026-08-20 10:52:31 -0400

    Add .gitignore

--- scent.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Scent Ledger Update"
-docker tag scent-ledger-scent-ledger ghcr.io/hxck/scent-ledger:latest
-docker push ghcr.io/hxck/scent-ledger:latest
-echo "Tagged and pushed. Restarting..."
-docker compose -f /opt/stacks/frags/compose.yaml pull
-docker compose -f /opt/stacks/frags/compose.yaml up -d
-echo "Finished."
